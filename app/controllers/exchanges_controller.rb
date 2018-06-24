2fe0b47d8f77d7ca494865a376a264b7958e4193
Vítor Marçal <git@vitormarcal.com.br>
2018-06-24 10:32:40 -0300

    deixado service imutavel

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,7 +3,7 @@ class ExchangesController < ApplicationController
   end
 
   def convert
-    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
+    value = ExchangeService.new.perform(params[:source_currency], params[:target_currency], params[:amount])
     render json: {"value": value}
   end
 end
--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -2,22 +2,16 @@ require 'rest-client'
 require 'json'
 
 class ExchangeService
-  def initialize(source_currency, target_currency, amount)
-    @source_currency = source_currency
-    @target_currency = target_currency
-    @amount = amount.to_f
-  end
 
-
-  def perform
+  def perform(source_currency, target_currency, amount)
     begin
       exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
       exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{source_currency}/#{target_currency}"
       res = RestClient.get url
       value = JSON.parse(res.body)['currency'][0]['value'].to_f
 
-      value * @amount
+      value * amount.to_f
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end
