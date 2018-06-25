0a3cf7cb932dedb2466b29d23bf7fe8b52129e31
Vítor Marçal <git@vitormarcal.com.br>
2018-06-25 09:25:05 -0300

    stub request

--- spec/support/api_data.rb
+++ spec/support/api_data.rb
@@ -1,5 +1,16 @@
 RSpec.configure do |config|
   config.before(:each) do
+    
+    stub_request(:get, "https://www.cryptonator.com/api/currencies").
+    with(
+      headers: {
+  	  'Accept'=>'*/*',
+  	  'Accept-Encoding'=>'gzip, deflate',
+  	  'Host'=>'www.cryptonator.com',
+  	  'User-Agent'=>'rest-client/2.0.2 (linux-gnu x86_64) ruby/2.5.1p57'
+      }).
+    to_return(status: 200, body: "", headers: {})
+    
     stub_request(:get, /currencydatafeed.com/ )
         .with(headers: {
             'Accept'=>'*/*'
