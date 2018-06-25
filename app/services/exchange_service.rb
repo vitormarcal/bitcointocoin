3b9b0e65d77cce4b265aa0d529856b0cb8882bf5
Vitor Marçal <vitormarcal8@gmail.com>
2018-06-25 12:28:24 -0300

    Adicionado logs

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -6,7 +6,9 @@ class ExchangeService
   def perform(source_currency, target_currency, amount)
     begin
       url = "https://api.cryptonator.com/api/ticker/#{source_currency}-#{target_currency}"
+      puts "GET to #{url}"
       res = RestClient.get url
+      puts "Response #{res}"
       value = JSON.parse(res.body)['ticker']['price'].to_f
       value * amount.to_f
     rescue RestClient::ExceptionWithResponse => e
