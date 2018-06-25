a40d3ea2803b769c204d5c8b6098b3c928fb3bc2
Vítor Marçal <git@vitormarcal.com.br>
2018-06-25 12:04:15 -0300

    Update api de conversão

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -5,12 +5,9 @@ class ExchangeService
 
   def perform(source_currency, target_currency, amount)
     begin
-      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{source_currency}/#{target_currency}"
+      url = "https://api.cryptonator.com/api/ticker/#{source_currency}-#{target_currency}"
       res = RestClient.get url
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
-
+      value = JSON.parse(res.body)['ticker']['price'].to_f
       value * amount.to_f
     rescue RestClient::ExceptionWithResponse => e
       e.response
