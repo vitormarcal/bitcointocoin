427c6b7f30c60c3741429260cf86103c3b065e10
Vítor Marçal <git@vitormarcal.com.br>
2018-06-26 22:02:11 -0300

    corrigido url teste

--- spec/support/api_data.rb
+++ spec/support/api_data.rb
@@ -9,10 +9,10 @@ RSpec.configure do |config|
     to_return(status: 200, body: '
     {
       "Data":
-              {
+             "BTC": {
                 "Symbol": "BTC"
               },
-              {
+             "USD": {
                 "Symbol": "USD"
              }
 
